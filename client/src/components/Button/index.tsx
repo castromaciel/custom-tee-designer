ee6bbb70b7f1c9be885f9f50ac2f888b4979271f
castromaciel <castromaciel00@gmail.com>
2023-10-08 16:23:00 -0300

    feat(client): add styles at custom button

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -1,9 +1,41 @@
-import { FC } from 'react'
+import state from '@/store'
+import { CSSProperties, FC, MouseEventHandler } from 'react'
+import { useSnapshot } from 'valtio'
 
-interface ButtonProps {}
+type ButtonType = 'filled'
 
-const Button: FC<ButtonProps> = () => (
-  <div>Button</div>
-)
+interface ButtonProps {
+  type: ButtonType
+  title: string
+  onClick: MouseEventHandler<HTMLButtonElement>
+  className?: string
+}
+
+const Button: FC<ButtonProps> = ({
+  onClick, title, type, className = ''
+}) => {
+  const snap = useSnapshot(state)
+
+  const generateStyle = (type: ButtonType): CSSProperties => {
+    if (type === 'filled') {
+      return {
+        backgroundColor: snap.color,
+        color: '#fff'
+      }
+    }
+
+    return {}
+  }
+
+  return (
+    <button
+      onClick={onClick}
+      className={`px-2 py-1.5 flex-1 rounded-md ${className}`}
+      style={generateStyle(type)}
+    >
+      {title}
+    </button>
+  )
+}
 
 export default Button
