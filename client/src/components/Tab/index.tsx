301298421582fd771a1e9848f99ecb484921bb5a
castromaciel <castromaciel00@gmail.com>
2023-10-09 00:41:15 -0300

    feat(client): customize Tab component

--- client/src/components/Tab/index.tsx
+++ client/src/components/Tab/index.tsx
@@ -1,19 +1,37 @@
-import { FC, MouseEventHandler } from 'react'
+import { useStore } from '@/hooks'
+import { CSSProperties, FC, MouseEventHandler } from 'react'
 
 interface TabProps {
   tab: {
     name: string
     icon: string
   }
-  onClick: MouseEventHandler<HTMLButtonElement>
+  onClick: MouseEventHandler<HTMLDivElement>
   isFilterTab?: boolean
   activeTab?: string
 }
 
-const Tab: FC<TabProps> = (props) => {
-  console.log(props)
+const Tab: FC<TabProps> = ({
+  onClick, tab, activeTab, isFilterTab
+}) => {
+  const snap = useStore()
+
+  const activeStyles: CSSProperties = isFilterTab && activeTab
+    ? { backgroundColor: snap.primaryColor, opacity: 0.5 }
+    : { backgroundColor: 'transparent', opacity: 1 }
+
   return (
-    <div>Tab</div>
+    <div
+      onClick={onClick}
+      className={`tab-btn ${isFilterTab ? 'rounded-full glasmorphism' : 'rounded-4'}`}
+      style={activeStyles}
+    >
+      <img 
+        src={tab.icon}
+        alt={tab.name}
+        className={`${isFilterTab ? 'w-2/3 h-2/3' : 'w-11/12 h-11/12 object-contain'}`}
+      />
+    </div>
   )
 }
 
