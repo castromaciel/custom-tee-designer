26fdbf33105b3c13c5ceb132266e251ff0ee1dee
castromaciel <castromaciel00@gmail.com>
2023-11-08 00:10:10 -0300

    feat(client): add ColorPicker component

--- client/src/components/ColorPicker/index.tsx
+++ client/src/components/ColorPicker/index.tsx
@@ -1,9 +1,27 @@
+import { useStore } from '@/hooks'
+import state from '@/store'
 import { FC } from 'react'
+import { ColorResult, SketchPicker } from 'react-color'
 
-interface ColorPickerProps {}
 
-const ColorPicker: FC<ColorPickerProps> = () => (
-  <div>ColorPicker</div>
-)
+interface ColorPickerProps { }
+
+const ColorPicker: FC<ColorPickerProps> = () => {
+  const snap = useStore()
+
+  const handleChange = (color: ColorResult) => {
+    state.primaryColor = color.hex
+  }
+
+  return (
+    <div className='absolute left-full ml-3'>
+      <SketchPicker
+        color={snap.primaryColor}
+        disableAlpha
+        onChange={handleChange}
+      />
+    </div>
+  )
+}
 
 export default ColorPicker
