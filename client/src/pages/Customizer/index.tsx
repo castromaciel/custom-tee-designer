d6698c8d4584ffc6bb248f7c8888728ff331c540
castromaciel <castromaciel00@gmail.com>
2023-11-19 12:34:55 -0300

    feat(client): update service

--- client/src/pages/Customizer/index.tsx
+++ client/src/pages/Customizer/index.tsx
@@ -71,7 +71,7 @@ const Customizer = () => {
 
     try {
       setGeneratingImg(true)
-      const response = await fetch('http://localhost:8000/api/v1/dalle', {
+      const response = await fetch('https://custom-tee-designer.onrender.com/api/v1/dalle', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
